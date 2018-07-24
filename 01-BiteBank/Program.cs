af95ef470676980ea02f7792f3bf410216259b39
wendriutavares <wendriu.fx@gmail.com>
2018-07-24 03:05:57 -0300

    Novos Projetos

--- 01-BiteBank/Program.cs
+++ 01-BiteBank/Program.cs
@@ -10,6 +10,23 @@ namespace _01_BiteBank
     {
         static void Main(string[] args)
         {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela H.";
+            contaDaGabriela.numero =  863452;
+            contaDaGabriela.agencia = 364;
+            contaDaGabriela.saldo = 1000;
+
+            Console.WriteLine("Dados da conta de: " + contaDaGabriela.titular);
+
+            Console.WriteLine("Titular: " + contaDaGabriela.titular);
+            Console.WriteLine("Agencia: " + contaDaGabriela.agencia);
+            Console.WriteLine("Numero " + contaDaGabriela.numero);
+            Console.WriteLine("Saldo: R$" + contaDaGabriela.saldo);
+
+
+            Console.ReadLine();
+                
         }
     }
 }
---
+++ 02-BayteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_BayteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Para iniciar, Tecle 1");
+            var acao = Console.ReadLine();
+            while (acao == "1")
+            {
+                ContaCorrente contaDoBruno = new ContaCorrente();
+
+                contaDoBruno.titular = "Bruno";
+
+                Console.WriteLine(contaDoBruno.saldo);
+
+                bool resultadoSaque = contaDoBruno.Sacar(500);
+
+                Console.WriteLine(contaDoBruno.saldo);
+                Console.WriteLine(resultadoSaque);
+
+            
+                Console.WriteLine("Para continuar, Tecle 1");
+
+                acao = Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
